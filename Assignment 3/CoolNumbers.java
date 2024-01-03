926807a70edc72e90414fdcd6947915e61156558
SebCompSci <sebcompsci@gmail.com>
2024-01-03 23:06:03 +0100

    Beginning of Assignment 3

---
+++ Assignment 3/CoolNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CoolNumbers {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isCool(int n, int a) {
+        String strANum = Integer.toString(a);
+
+        for (int i = 0; i < strANum.length(); i++) {
+            if (Character.getNumericValue(strANum.charAt(i)) != 0) {
+                
+
+            }
+        }
+    }
+
+    public static int countsCools(int n, int a, int b) {
+        
+    }
+}
