e84fd93ebe6e3626c881603ed4049d7ec44adf24
SebCompSci <sebcompsci@gmail.com>
2024-01-04 21:20:22 +0100

    Assignment 3 Complete

--- Assignment 3/CoolNumbers.java
+++ Assignment 3/CoolNumbers.java
@@ -1,22 +1,44 @@
-import java.util.Scanner;
+/*
+ * Assignment 3: Cool Numbers.
+ * The program should read 3 integers (a, b, and n), given 'n', the program will count all possible 'cool' numbers between 'a' and 'b'.
+ * Given a (non-zero) positive integer 'n' a number is considered 'cool' if 'n' is divisible by each of the number's digit without remainder.
+ */
 
 public class CoolNumbers {
-    public static void main(String[] args) {
 
-    }
 
-    public static boolean isCool(int n, int a) {
-        String strANum = Integer.toString(a);
+    public static boolean isCool(int a, int n) {
+        //Takes int a and int n.
+        //'a' is considered a cool number if 'n' is divisable by every digit of 'a' without remainder.
+        //Returns a boolean (remainder), true if a is cool number, and false when not. 
 
-        for (int i = 0; i < strANum.length(); i++) {
-            if (Character.getNumericValue(strANum.charAt(i)) != 0) {
-                
-
+        String aStr = Integer.toString(a);
+        boolean noRemainder = true;
+        for (int i = 0; i < aStr.length(); i++) {
+            int aChar = Character.getNumericValue(aStr.charAt(i)); //Iterates over digits of 'a'.
+            if(noRemainder == true) {
+                if (aChar == 0){
+                    noRemainder = false;
+                } else if ((n % aChar) != 0) {
+                    noRemainder = false;
+                }
             }
         }
+        return noRemainder;
     }
 
-    public static int countsCools(int n, int a, int b) {
-        
+
+    public static int countCools(int n, int a, int b) {
+        //Takes int n, int a, and int b.
+        //Counts how many cool number exist for 'n' between 'a' and 'b'.
+        //Uses 'isCool' method to check if the number is a cool number.
+
+        int coolCount = 0;
+        for (int i = a; i <= b; i++) {
+            if (isCool(i, n) == true) {
+                coolCount ++;
+            }
+        }
+        return coolCount;
     }
 }
